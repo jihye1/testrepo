57749c3f18dbf0f385d79922deacc4d90cf7130c
yeongtaik byeon <yt.byeon@samsung.com>
2018-01-26 16:45:54 +0900

    Update cwe.js

--- js/cwe.js
+++ js/cwe.js
@@ -16,9 +16,8 @@ function CWE_398() { // IDENTICAL_BRANCHES
 }
 
 function CWE_476() { // NULL_POINTER
-    var obj = {};
+    var obj;
     var z = obj.y; 
-    var y = obj.z;
     console.log(z); 
 }
 
